d26fc854f80f0eeea15ad73b190c4bd53200837e
Harsh Patel <Patelharsh241999@gmail.com>
2023-09-18 14:40:53 +0530

    * Updated background based on url.

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,11 @@
-import { Suspense } from "preact/compat";
+import { Suspense, useEffect } from "preact/compat";
 import { Loading, TicTacToe } from "./components";
 
 export function App() {
+  useEffect(()=>{
+    const isIframeUrl = window.location.href.includes("iframeUrl")
+    document.body.style.background=isIframeUrl?"transparent":"#1c1917"
+  },[])
   return (
     <Suspense fallback={<Loading />}>
       <TicTacToe />
